8848e633eb09a4add3d690091dda4a70fa6ac7d5
Joshua Martin <resume@joshmartin.me>
2016-08-01 15:01:41 -0500

    Add project files.

---
+++ PokerScoring.Core/Poker.fs
@@ -0,0 +1,148 @@
+﻿module Poker
+
+open System.Collections.Generic
+
+type Suit =
+    | Spades
+    | Diamonds
+    | Clubs
+    | Hearts
+
+type Card =
+    | Ace of Suit
+    | King of Suit
+    | Queen of Suit
+    | Jack of Suit
+    | Pip of int * Suit 
+
+type Score =
+    | RoyalFlush 
+    // all other scores should contain the highest card
+    // of the match - in case of a tie (doesn't apply to Royal flush).
+    | StraightFlush of Card
+    | FourOfAKind of Card
+    | FullHouse of Card
+    | Flush of Card
+    | Straight of Card
+    | ThreeOfAKind of Card
+    | TwoPair of Card
+    | Pair of Card
+    | HighCard of Card
+
+let CreateSuite = function 
+    | 0 -> Spades
+    | 1 -> Diamonds
+    | 2 -> Clubs
+    | 3 -> Hearts
+    | _ -> failwith "cannot create suite"
+
+let CreateCard number suite = 
+    match number with 
+    | 11 -> Jack(suite)
+    | 12 -> Queen(suite)
+    | 13 -> King(suite)
+    | 14 -> Ace(suite)
+    | i when 1 < i && i < 11 -> Pip(i, suite)
+    | _ -> failwith "cannot create card"
+
+// returns integer value of Card
+let CardValue = function
+    | Ace(s) -> 14
+    | King(s) -> 13
+    | Queen(s) -> 12
+    | Jack(s) -> 11
+    | Pip(i,s) -> i
+
+// returns Suite of Card
+let CardSuite = function
+    | Ace(s) -> s
+    | King(s) -> s
+    | Queen(s) -> s
+    | Jack(s) -> s
+    | Pip(i,s) -> s
+
+let CreateDeck = [ for s in 0..3 do for v in 2..14 -> CreateSuite s |> CreateCard v ]
+
+// Input Card list, shuffles and outputs a Stack object
+let ShuffleDeck cards = 
+    let rnd = new System.Random()
+    cards |> List.sortBy (fun i -> rnd.Next()) |> Stack
+    
+let DrawHand (deck: Stack<_>) = [ for i in 0..4 -> deck.Pop() ]
+         
+let GetCardString = function
+    | Ace(s) -> sprintf "Ace of %s" (s.ToString())
+    | King(s) -> sprintf "King of %s" (s.ToString())
+    | Queen(s) -> sprintf "Queen of %s" (s.ToString())
+    | Jack(s) -> sprintf "Jack of %s" (s.ToString())
+    | Pip(i,s) -> sprintf "%i of %s" i (s.ToString())
+
+let private GetRankedCard op cards = 
+    cards 
+        |> List.tail 
+        |> List.fold (fun c1 c2 -> if op (CardValue c1) (CardValue c2) then c1 else c2) (List.head(cards))
+
+let GetHighCard = List.ofSeq >> GetRankedCard (>)
+
+let GetLowCard = List.ofSeq >> GetRankedCard (<)
+
+let (|Sequential|_|) cards =
+    let orderedCardValues = cards |> List.ofSeq |> List.map CardValue |> List.sort
+    // can this be refactored to use fold?
+    let rec folder current cardVals =
+        match cardVals with
+            // if the collection is empty, then = sequential.
+            // since Ace can be a high card OR a 1 in a straight, check if last item is an Ace and current is a 5 or 13 = sequential
+            | [] | [14] when current = 5 || current = 13 -> cards |> GetHighCard |> Some 
+            | head :: vals when (current+1) = head -> folder head vals
+            | _ -> None
+    // returns Some(Card) where Card is the highest card of the sequence, or None (not sequential)
+    folder (List.head(orderedCardValues)) (List.tail(orderedCardValues))
+  
+// returns Some(Card) where Card is the highest card of the match, or None (not one of a kind)
+let (|OneOfAKind|_|) cards = 
+    if cards 
+        |> Seq.map CardSuite 
+        |> Seq.distinct 
+        |> Seq.length = 1 then cards |> GetHighCard |> Some else None
+
+// i = num matching cards, k = number of matches 
+// returns Some(Card) where Card is the highest card of the match, or None (no matches)
+let (|HasMatch|_|) i k cards = 
+    let matches = 
+        cards 
+            |> Seq.groupBy (fun i -> CardValue i)
+            |> Seq.filter (fun (k, v) -> v |> Seq.length = i)        
+    if matches |> Seq.length = k then matches |> Seq.head |> (fun (k, v) -> v |> GetHighCard) |> Some else None
+
+let GetHandScore (cards:Card seq) = 
+    match cards with
+    | Sequential c & OneOfAKind _ -> if cards |> GetLowCard |> CardValue = 10 then RoyalFlush else StraightFlush c
+    | HasMatch 4 1 c -> FourOfAKind c
+    | HasMatch 3 1 c & HasMatch 2 1 _ -> FullHouse c
+    | OneOfAKind c -> Flush c
+    | Sequential c -> Straight c
+    | HasMatch 3 1 c -> ThreeOfAKind c
+    | HasMatch 2 2 c -> TwoPair c
+    | HasMatch 2 1 c -> Pair c
+    | _ -> cards |> GetHighCard |> HighCard
+
+
+// EXTRAS
+
+type Player = { Name: string; Hand: Card list }
+
+let (|HighestCardValue|) (StraightFlush c|FourOfAKind c|FullHouse c|Flush c|Straight c|ThreeOfAKind c|TwoPair c|Pair c|HighCard c) = CardValue c
+
+// returns tuple (player, score, integer val of highest card of score)
+let private decompose player =
+    let score = player.Hand |> GetHandScore
+    (player, score, match score with | RoyalFlush -> 0 | HighestCardValue c -> c)
+
+let GetWinningHand players =
+    players 
+    |> List.map decompose
+    // sort by Score asc, then by highest card value desc
+    |> List.sortBy (fun (p, s, c) -> s, -c) 
+    |> List.map (fun (p, s, c) -> p) 
+    |> List.head
---
+++ PokerScoring.Test/Tests.fs
@@ -0,0 +1,171 @@
+﻿namespace PokerScoring.Test
+
+open System.Collections.Generic
+open Xunit
+open Poker
+
+module Tests =
+    [<Fact>]
+    let ``Can Create Standard 52 deck``() = 
+        let deck = CreateDeck
+        Assert.True(deck |> Seq.length = 52)
+
+    [<Fact>]
+    let ``Can Shuffle Deck``() = 
+        let deck1 = CreateDeck 
+        let deck2 = CreateDeck |> ShuffleDeck
+        Assert.False(deck1 |> Seq.zip deck2 |> Seq.forall (fun (a, b) -> a = b))
+
+    [<Fact>]
+    let ``Can Draw Hand``() = 
+        let cards = CreateDeck |> ShuffleDeck |> DrawHand
+        Assert.True(cards |> Seq.length = 5)
+
+    [<Fact>]
+    let ``Can Draw Card``() = 
+        let cards = [Pip(4, Spades); Pip(6, Hearts); Ace(Hearts); Jack(Hearts); Pip(4, Clubs)] |> Stack
+        let topCard = cards.Pop()
+        Assert.True((Pip(4, Clubs)) = topCard)
+
+    [<Fact>]
+    let ``Can Get High Card``() = 
+        let highCard = [Pip(4, Spades); Pip(6, Hearts); Ace(Hearts); Jack(Hearts); Pip(4, Clubs)] |> GetHighCard
+        Assert.True((Ace(Hearts)) = highCard)
+
+    [<Fact>]
+    let ``Can Get Low Card``() = 
+        let lowCard = [Pip(2, Spades); Pip(6, Hearts); Ace(Hearts); Jack(Hearts); Pip(4, Clubs)] |> GetLowCard
+        Assert.True((Pip(2, Spades)) = lowCard)
+
+    [<Fact>]
+    let ``Can Score Royal Flush``() = 
+        let deck = [Ace(Spades); King(Spades); Queen(Spades); Jack(Spades); Pip(10, Spades)] |> ShuffleDeck 
+        let hand = deck |> DrawHand
+        Assert.True(deck |> Seq.length = 0)
+        match hand |> GetHandScore with
+            | RoyalFlush -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Straight Flush``() = 
+        let score = [Pip(2, Spades); Pip(3, Spades); Ace(Spades); Pip(4, Spades); Pip(5, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | StraightFlush c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Flush``() = 
+        let score = [Pip(2, Spades); Pip(3, Spades); Ace(Spades); Pip(7, Spades); Pip(8, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | Flush c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score High Card``() = 
+        let score = [Pip(9, Spades); Pip(3, Spades); Ace(Hearts); Pip(4, Spades); Pip(5, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | HighCard c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Two Pairs``() = 
+        let score = [Pip(2, Spades); Pip(2, Hearts); Ace(Spades); Pip(5, Hearts); Pip(5, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | TwoPair c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Pair``() = 
+        let score = [Pip(2, Spades); Pip(2, Hearts); Ace(Spades); Pip(3, Hearts); Pip(5, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | Pair c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Three of a Kind``() = 
+        let score = [Pip(2, Spades); Pip(2, Hearts); Pip(2, Diamonds); Pip(4, Spades); Pip(5, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | ThreeOfAKind c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Four of a Kind``() = 
+        let score = [Pip(2, Spades); Pip(2, Hearts); Pip(2, Diamonds); Pip(2, Clubs); Pip(5, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | FourOfAKind c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Full House``() = 
+        let score = [Pip(2, Spades); Pip(2, Hearts); Pip(2, Diamonds); Pip(10, Clubs); Pip(10, Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | FullHouse c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Straight (10,J,Q,K,A)``() = 
+        let score = [Pip(10, Spades); Jack(Hearts); Queen(Diamonds); King(Clubs); Ace(Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | Straight c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Score Straight (A,2,3,4,5)``() = 
+        let score = [Pip(2, Hearts); Pip(3, Spades); Pip(4, Spades); Pip(5, Spades); Ace(Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | Straight c -> ()
+            | _ -> failwith "Failed"
+
+    [<Fact>]
+    let ``Can Not Score Straight (A,2,3,Q,K)``() = 
+        let score = [Pip(2, Hearts); Pip(3, Spades); King(Spades); Queen(Spades); Ace(Spades)] |> ShuffleDeck |> DrawHand |> GetHandScore
+        match score with
+            | HighCard c -> ()
+            | _ -> failwith "Failed"
+
+
+
+// EXTRAS
+    let player1 = { Name = "Straight Flush (AS,2H,3S,4S,5S)"; Hand = [Pip(2, Spades); Pip(3, Spades); Pip(4, Spades); Pip(5, Spades); Ace(Spades)] }
+    let player2 = { Name = "Royal Flush (10S,JS,QS,KS,AS)"; Hand = [Pip(10, Spades); Jack(Spades); Queen(Spades); King(Spades); Ace(Spades)] }
+    let player3 = { Name = "Four of a Kind (AS,AH,AC,AD,5S)"; Hand = [Pip(2, Hearts); Ace(Spades); Ace(Hearts); Ace(Clubs); Ace(Diamonds)] }
+    let player4 = { Name = "Flush (AS,2H,3S,4S,5S)"; Hand = [Pip(9, Spades); Pip(3, Spades); Pip(4, Spades); Pip(5, Spades); Ace(Spades)] }
+    let player5 = { Name = "Straight (AS,2H,3S,4S,5S)"; Hand = [Pip(2, Hearts); Pip(3, Spades); Pip(4, Spades); Pip(5, Spades); Ace(Spades)] }
+    let player6 = { Name = "Low Pair (AS,2H,2S,4S,5S)"; Hand = [Pip(2, Spades); Pip(2, Hearts); Pip(4, Spades); Pip(5, Spades); Ace(Spades)] }
+    let player7 = { Name = "High Pair (2H,3S,4S,AH,AS)"; Hand = [Pip(2, Hearts); Pip(3, Spades); Pip(4, Spades); Ace(Hearts); Ace(Spades)] }
+
+    [<Fact>]
+    let ``Winning hand (Royal Flush)``() = 
+        let players = [ player1; player2; player3; player4; player5; player6; player7 ]
+        let winner = players |> GetWinningHand 
+        Assert.True(winner.Name = player2.Name)
+
+    [<Fact>]
+    let ``Winning hand (Straight Flush)``() = 
+        let players = [ player1; player3; player4; player5; player6; player7 ]
+        let winner = players |> GetWinningHand 
+        Assert.True(winner.Name = player1.Name)
+
+    [<Fact>]
+    let ``Winning hand (Four of a Kind)``() = 
+        let players = [ player3; player4; player5; player6; player7 ]
+        let winner = players |> GetWinningHand 
+        Assert.True(winner.Name = player3.Name)
+
+    [<Fact>]
+    let ``Winning hand (Flush)``() = 
+        let players = [ player4; player5; player6; player7 ]
+        let winner = players |> GetWinningHand 
+        Assert.True(winner.Name = player4.Name)
+
+    [<Fact>]
+    let ``Winning hand (Straight)``() = 
+        let players = [ player5; player6; player7 ]
+        let winner = players |> GetWinningHand 
+        Assert.True(winner.Name = player5.Name)
+
+    [<Fact>]
+    let ``Winning hand (High Pair)``() = 
+        let players = [ player6; player7 ]
+        let winner = players |> GetWinningHand 
+        Assert.True(winner.Name = player7.Name)
